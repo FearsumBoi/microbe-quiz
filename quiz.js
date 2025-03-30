cc92b78bc769774933bfa382bbe67998df057c3c
FearsumBoi <57815641+FearsumBoi@users.noreply.github.com>
2025-03-30 19:28:31 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -33,8 +33,12 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  function shuffleQuestions(questions) {
-    return questions.sort(() => Math.random() - 0.5);
+function shuffleQuestions(questions) {
+    for (let i = questions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [questions[i], questions[j]] = [questions[j], questions[i]];
+    }
+    return questions;
   }
 
   function startQuiz() {
